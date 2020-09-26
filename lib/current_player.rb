480173cbeaedd64037e2786d8eb28217bc755d0a
Christopher Kim <chris.kim1990@gmail.com>
2020-09-26 16:11:28 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ def turn_count(board)
   counter = 0
   board.each do |turn|
 
-    if turn.upcase == "X" || turn == "O"
+    if turn.upcase == "X" || turn.upcase == "O"
       counter += 1
     end
   end
